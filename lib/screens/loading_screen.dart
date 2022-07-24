91b758d3c16edea0741e8dca1423ebb6e5c2d923
luciferro01 <lucifer1111ronaldo@gmail.com>
2022-07-24 17:01:56 +0530

    Adding location and http request with open Weather in the Application-Clima

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: library_private_types_in_public_api, avoid_print
 import 'package:flutter/material.dart';
+import '../services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apiKey = '4cad1f6858687dd6a48dbaebe24d2761';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -8,15 +14,59 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    gettingLocation();
+  }
+
+  void gettingLocation() async {
+    GetLocation location = GetLocation();
+    await location.getLocation();
+    // print(location.latitude);
+    // print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+    getData();
+  }
+
+  void getData() async {
+    String url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
+    http.Response response = await http.get(
+      Uri.parse(url),
+    );
+    // print(response.body);
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodeData = jsonDecode(data);
+      var temperature = decodeData['main']['temp'];
+      var condition = decodeData['weather'][0]['main'];
+      var cityName = decodeData['name'];
+
+      print(temperature);
+      print(condition);
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
+            // getData();
+            gettingLocation();
             //Get the current location
           },
-          child: Text('Get Location'),
+          child: const Text('Get Location'),
         ),
       ),
     );
